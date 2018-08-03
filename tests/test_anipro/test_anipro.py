87ac16c4a299cb095893b839b949bbb6781d18ac
Gan Zhaoyu <ganzhaoyu037@gmail.com>
2018-08-03 21:15:20 +0800

    Fixed compatibility issue of capsys in pytest

--- tests/test_anipro/test_anipro.py
+++ tests/test_anipro/test_anipro.py
@@ -7,10 +7,20 @@ def test_bar_progress(capsys):
     ap.start_bar_progress('Bar Test Text')
     time.sleep(5.1)
     ap.stop_progress()
-    assert capsys.readouterr().out == 'Bar Test Text...........\n'
+    outerr = capsys.readouterr()
+    if isinstance(outerr, tuple):
+        out = outerr[0]
+    else:
+        out = outerr.out
+    assert out == 'Bar Test Text...........\n'
 
 def test_rotate_progress(capsys):
     ap.start_rotated_progress('Rotated Test Text')
     time.sleep(2.1)
     ap.stop_progress()
-    assert capsys.readouterr().out == 'Rotated Test Text \b-\b\\\b|\b/\b-\n'
+    outerr = capsys.readouterr()
+    if isinstance(outerr, tuple):
+        out = outerr[0]
+    else:
+        out = outerr.out
+    assert out == 'Rotated Test Text \b-\b\\\b|\b/\b-\n'
